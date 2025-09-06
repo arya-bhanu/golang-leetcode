479e4a354d63fad5d898c7e652767198cdc29088
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-09-06 16:29:12 +0700

    feat: [FASTEST]

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func SolveProblem() string {
-	return "Hello World"
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	tests := []int{3, 2, 1, 10, 12, 200, 100}
+	sort.Ints(tests)
+	fmt.Println(tests)
+}
+
+func getSneakyNumbers(nums []int) []int {
+	var duplicate []int
+	sort.Ints(nums)
+	for i, val := range nums {
+		if i+1 < len(nums) {
+			if val == nums[i+1] {
+				duplicate = append(duplicate, val)
+			}
+		}
+	}
+	return duplicate
 }
--- main_test.go
+++ main_test.go
@@ -7,5 +7,7 @@ import (
 )
 
 func TestMainFunc(t *testing.T) {
-	assert.Equal(t, "Hello World", SolveProblem())
+	assert.ElementsMatch(t, []int{0, 1}, getSneakyNumbers([]int{0, 1, 1, 0}))
+	assert.ElementsMatch(t, []int{2, 3}, getSneakyNumbers([]int{0, 3, 2, 1, 3, 2}))
+	assert.ElementsMatch(t, []int{4, 5}, getSneakyNumbers([]int{7, 1, 5, 4, 3, 4, 6, 0, 9, 5, 8, 2}))
 }
