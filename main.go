000336c9c84ffe30b6f5766fcbb52f0f597fd03f
putu-bhanuartha <putu.bhanuartha@paper.id>
2025-09-07 09:30:44 +0700

    done: 100% fastest

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func SolveProblem() string {
-	return "Hello World"
+func numIdenticalPairs(nums []int) int {
+	counter := 0
+	for i, val := range nums {
+		j := i + 1
+		for j < len(nums) {
+			if val == nums[j] {
+				counter++
+			}
+			j++
+		}
+	}
+	return counter
 }
--- main_test.go
+++ main_test.go
@@ -7,5 +7,7 @@ import (
 )
 
 func TestMainFunc(t *testing.T) {
-	assert.Equal(t, "Hello World", SolveProblem())
+	assert.Equal(t, 4, numIdenticalPairs([]int{1, 2, 3, 1, 1, 3}))
+	assert.Equal(t, 6, numIdenticalPairs([]int{1, 1, 1, 1}))
+	assert.Equal(t, 0, numIdenticalPairs([]int{1, 2, 3}))
 }
